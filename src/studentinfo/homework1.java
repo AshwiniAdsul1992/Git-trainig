be5004e57f3223a70edbfc9cc2ffbedc084ddcc7
AshwiniAdsul1992 <ashwiniadsul22@gmail.com>
2022-12-14 23:20:53 +0530

    second commit

---
+++ src/studentinfo/homework1.java
@@ -0,0 +1,29 @@
+package studentinfo;
+
+public class homework1 
+    {
+	
+	public static void main( String[] arg)
+	{
+		String studentname = "Ashwini";
+		String schoolname = "Elite school";
+		String address  = "Pune";
+		byte rollnumber = 10;
+		float percentage = 80.91f;
+		int totalmarks = 453;
+		char grade = 'A';
+		String result = "pass";
+		
+		
+		System.out.println(studentname);
+		System.out.println(schoolname);
+		System.out.println(address);
+		System.out.println(rollnumber);
+		System.out.println(percentage+"%");
+		System.out.println(totalmarks);
+		System.out.println(grade);
+		System.out.println(result);
+		System.out.println(studentname);
+	}
+
+}
---
+++ src/studentinfo/studentinfo.java
@@ -0,0 +1,38 @@
+package studentinfo;
+
+public class studentinfo 
+{
+	public static void main( String[] arg)
+	{
+		short s;
+		int i;
+		long lg;
+		float fl;
+		double d;
+		char c;
+		boolean b; 
+		String str;
+		
+		s = 3454;
+		i = 45667;
+		lg = 1344557785l;
+		fl = 54.87f;
+		d = 566788.09876;
+		c = 'd';
+		b = true;
+		str = "pune";
+		
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(lg);
+		System.out.println(fl);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(str);
+		System.out.println(11);
+		System.out.println(20);
+		System.out.println("ashu");
+	    System.out.println(200);
+		}
+}
