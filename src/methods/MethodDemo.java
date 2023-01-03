35a41faa8946fefe0da8e39282e7142272393805
AshwiniAdsul1992 <ashwiniadsul22@gmail.com>
2023-01-03 14:30:57 +0530

    hi

---
+++ src/methods/MethodDemo.java
@@ -0,0 +1,36 @@
+package methods;
+
+public class MethodDemo 
+   {
+	 public static void display()
+	 {
+		 System.out.println("welcome to my home");
+		 
+		 show();
+		 
+	 }
+     public static void show ()
+     {
+    	 System.out.println ("im living in mumbai");
+    	 
+    }
+      public static void main (String[] arg)
+      
+      {
+    	  
+    	  display();
+    	  
+    	  show ();
+    	  
+    	  display();
+    	  
+    	  show ();
+    	  	  
+    	  
+    
+   }
+      
+		 
+	 }
+   
+	
